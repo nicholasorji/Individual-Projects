98da2dcefc0302896b44cec515e532fa642f3435
nicholas343 <nicholas.orji25@gmail.com>
2018-01-06 23:36:33 -0500

    Added file

---
+++ Login.java
@@ -0,0 +1,27 @@
+package New;
+
+import java.util.Scanner;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+String user;
+String pass;
+
+Scanner scan = new Scanner(System.in);
+Scanner scana = new Scanner(System.in);
+
+System.out.print("Enter you username:");
+String n = scan.nextLine();
+System.out.print("Enter you password:");
+String a = scan.nextLine();
+
+
+
+		
+	}
+
+}
