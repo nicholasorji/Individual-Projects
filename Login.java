94c4f4f3567978f38e360e9a7af258ba731ea540
nicholas343 <nicholas.orji25@gmail.com>
2018-01-15 03:45:26 -0500

    Made edits to login system

--- Login.java
+++ Login.java
@@ -4,24 +4,47 @@ import java.util.Scanner;
 
 public class Login {
 
-	public static void main(String[] args) {
+static String user;
+static String pass;
+static String loginuser;
+static String loginpass;
+static boolean login = true;
+static boolean check = false;
+static Scanner scan = new Scanner(System.in);
+
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 
-String user;
-String pass;
-
-Scanner scan = new Scanner(System.in);
-Scanner scana = new Scanner(System.in);
-
-System.out.print("Enter you username:");
-String n = scan.nextLine();
-System.out.print("Enter you password:");
-String a = scan.nextLine();
-
-
-
+	System.out.print("Enter you username:");
+	String loginuser = scan.nextLine();
+	System.out.print("Enter you password:");
+	String loginpass = scan.nextLine();
+	
+	
+	System.out.print("Thank you for logging in");
+	while(login) {
 		
+		check = false;
+		System.out.println(" ------------------ \n" + "Username: \n");
+        user = scan.nextLine();
+        System.out.println("Password: \n");
+        pass = scan.nextLine();
+		
+        if(loginuser.equals(user) && (loginpass.equals(pass))) {
+        
+        	check = true;
+        login = false;
+        System.out.print("Success");
+        }
+        else {
+        	
+        	check = false;
+        	System.out.print("Incorrect. Please try again");
+        	
+        }
 	}
+	
+}
 
 }
